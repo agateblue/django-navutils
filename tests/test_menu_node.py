abff136e304972320115a2bf3d476636440378cf
Eliot Berriot <contact@hat.eliotberriot.com>
2015-05-16 20:49:51 +0200

    Added weight for menu nodes

--- tests/test_menu_node.py
+++ tests/test_menu_node.py
@@ -14,3 +14,25 @@ class MenuNodeTest(TestCase):
     def test_menu_node_allows_django_route_with_kwargs(self):
         menu_node = MenuNode('Example', route='category', reverse_kwargs=['slug'])
         self.assertEqual(menu_node.get_url(slug='test'), '/category/test')
+
+    def test_menu_node_sort_children_by_weight(self):
+        child1 = MenuNode('Child1', weight=3, url='http://example.com/child1')
+        child2 = MenuNode('Child2', weight=1, url='http://example.com/child2')
+        child3 = MenuNode('Child3', weight=2, url='http://example.com/child3')
+
+        parent = MenuNode(
+            'Example',
+            url='http://example.com',
+            children=[
+                child1,
+                child2,
+                child3,
+            ]
+        )
+
+        self.assertEqual(parent.children, [child1, child3, child2])
+
+        child4 = MenuNode('Child4', weight=999, url='http://example.com/child4')
+        parent.add(child4)
+
+        self.assertEqual(parent.children, [child4, child1, child3, child2])
