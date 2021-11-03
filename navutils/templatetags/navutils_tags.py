8e115f0897962a79521808ef3b83c54ccdf287d4
Kevin Ramirez <kevin.ramirez@mandarinprojects.mx>
2021-11-03 11:31:52 -0600

    order parent nodes render_menu tag

--- navutils/templatetags/navutils_tags.py
+++ navutils/templatetags/navutils_tags.py
@@ -17,9 +17,10 @@ def render_menu(context, menu, **kwargs):
 
     max_depth = kwargs.get('max_depth', context.get('max_depth', 999))
     viewable_nodes = [node for node in menu.values() if node.is_viewable_by(user, context)]
-    print('viewable_nodes', viewable_nodes)
-    sorted_viewable_nodes = sorted(viewable_nodes,key=lambda i: i.weight, reverse=True)
-    print('sorted_viewable_nodes', sorted_viewable_nodes)
+    
+    # Also sort parent nodes if them have weight
+    viewable_nodes = sorted(viewable_nodes,key=lambda i: i.weight, reverse=True)
+
     if not viewable_nodes:
         return ''
 
