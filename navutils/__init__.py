c42b5bc3b485658b44fb135c37712d4b64378be8
powderflask <powderflask@gmail.com>
2022-05-05 15:19:41 -0700

    #20 django3.2 compatibility

--- navutils/__init__.py
+++ navutils/__init__.py
@@ -1,7 +1,9 @@
+import django
 
 from .breadcrumbs import Breadcrumb, BreadcrumbsMixin
 from .views import MenuMixin
 
 from . import menu
 
-default_app_config = 'navutils.apps.App'
+if django.VERSION < (3, 2):
+    default_app_config = 'navutils.apps.App'
